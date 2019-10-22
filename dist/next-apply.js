f2774d655c87e9bd84072b97864d31c02088b7e7
afeiship <1290657123@qq.com>
2019-10-22 10:29:47 +0800

    add: basic unit test

--- dist/next-apply.js
+++ dist/next-apply.js
@@ -2,7 +2,7 @@
  * name: next-apply
  * url: https://github.com/afeiship/next-apply
  * version: 1.0.0
- * date: 2019-10-22T02:14:08.347Z
+ * date: 2019-10-22T02:29:33.151Z
  * license: MIT
  */
 
@@ -11,19 +11,19 @@
   var nx = global.nx || require('next-js-core2');
   var FUNC = 'function';
 
-  nx.apply = function(inFunction, inContext, inArgs) {
-    if (typeof inFunction !== FUNC) return;
+  nx.apply = function(inFunc, inContext, inArgs) {
+    if (typeof inFunc !== FUNC) return;
     switch (inArgs ? inArgs.length : 0) {
       case 0:
-        return inFunction.call(inContext);
+        return inFunc.call(inContext);
       case 1:
-        return inFunction.call(inContext, inArgs[0]);
+        return inFunc.call(inContext, inArgs[0]);
       case 2:
-        return inFunction.call(inContext, inArgs[0], inArgs[1]);
+        return inFunc.call(inContext, inArgs[0], inArgs[1]);
       case 3:
-        return inFunction.call(inContext, inArgs[0], inArgs[1], inArgs[2]);
+        return inFunc.call(inContext, inArgs[0], inArgs[1], inArgs[2]);
       default:
-        return inFunction.apply(inContext, inArgs);
+        return inFunc.apply(inContext, inArgs);
     }
   };
 
--- src/next-apply.js
+++ src/next-apply.js
@@ -3,19 +3,19 @@
   var nx = global.nx || require('next-js-core2');
   var FUNC = 'function';
 
-  nx.apply = function(inFunction, inContext, inArgs) {
-    if (typeof inFunction !== FUNC) return;
+  nx.apply = function(inFunc, inContext, inArgs) {
+    if (typeof inFunc !== FUNC) return;
     switch (inArgs ? inArgs.length : 0) {
       case 0:
-        return inFunction.call(inContext);
+        return inFunc.call(inContext);
       case 1:
-        return inFunction.call(inContext, inArgs[0]);
+        return inFunc.call(inContext, inArgs[0]);
       case 2:
-        return inFunction.call(inContext, inArgs[0], inArgs[1]);
+        return inFunc.call(inContext, inArgs[0], inArgs[1]);
       case 3:
-        return inFunction.call(inContext, inArgs[0], inArgs[1], inArgs[2]);
+        return inFunc.call(inContext, inArgs[0], inArgs[1], inArgs[2]);
       default:
-        return inFunction.apply(inContext, inArgs);
+        return inFunc.apply(inContext, inArgs);
     }
   };
 
